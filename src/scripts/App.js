5242d5d9e88b3e0244532140d1831c24ed4cdc45
Erik Waters <erikj.waters@gmail.com>
2019-05-21 14:47:45 -0400

    renders data

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,119 +1,149 @@
-import React from 'react';
-import '../../style.css'
-
-const data = require('../../schema.json')
-
+import React from "react";
+import "../../style.css";
+const data = require("../../schema.json");
+// ----- Utility Functions
+// creates human readable name from elements' name property
 const formatReadableName = name => {
-  const convertToProper = s => s.charAt(0).toUpperCase() + s.slice(1)
+  const convertToProper = s => s.charAt(0).toUpperCase() + s.slice(1);
   // handle edge cases
-  if (name.slice(0,4) ==='name') {
-    if (name === 'name_nick') return 'Nickname'
-    return name.indexOf('fix') > 0
+  if (name === "lat") return "Latitude";
+  if (name === "lng") return "Longitude";
+  if (name.slice(0, 4) === "name") {
+    if (name === "name_nick") return "Nickname";
+    return name.indexOf("fix") > 0
       ? convertToProper(name.slice(5))
-      : convertToProper(name.slice(5)) + ' Name'
+      : convertToProper(name.slice(5)) + " Name";
   }
-  return name.indexOf('_') > 0
-   // capitalize and convert '_' to ' '
-   ? name.split('_')
-    .map((subString) => {
-      switch(subString) {
-        case 'et':
-          return 'EverTrue'
-          break;
-        case 'dt':
-          return 'Date'
-          break;
-        case 'lq':
-          return 'Lower Quartile'
-          break;
-        case 'uq':
-          return 'Upper Quartile'
-        default:
-          return convertToProper(subString)
+  return name.indexOf("_") > 0
+    ? // capitalize and convert underscore '_' to space ' '
+      name
+        .split("_")
+        .map(subString => {
+          switch (subString) {
+            case "et":
+              return "EverTrue";
+            case "dt":
+              return "Date";
+            case "lq":
+              return "Lower Quartile";
+            case "uq":
+              return "Upper Quartile";
+            default:
+              return convertToProper(subString);
+          }
+        })
+        .join(" ")
+    : convertToProper(name);
+};
+// rebuilds data structure to match sidenav component hierarchy
+const massageData = data =>
+  data.reduce(
+    (acc, cur) => {
+      if (cur.containing_object) {
+        // spread properties to top level object
+        acc[cur.containing_object.name] = { ...cur.containing_object };
+      } else if (cur.properties) {
+        // as above
+        acc[cur.name] = { ...cur };
+      } else {
+        // put individual elements into general info object
+        acc.general.properties.push({ ...cur });
       }
-    }).join(' ')
-  : convertToProper(name)
-}
-
-const massageData = data => data.reduce((acc, cur) => {
-  if (cur.containing_object) {
-    // spread properties to top level object
-    acc[cur.containing_object.name] = {...cur.containing_object}
-  } else if (cur.properties) {
-    // as above
-    acc[cur.name] = {...cur}
-  // put individual elements into general info object
-  } else {
-    acc.general.properties.push({...cur})
-  }
-  return acc
-}, {general: {properties: []}})
-
-const formattedData = massageData(data)
-
+      return acc;
+    },
+    { general: { properties: [], name: "general" } }
+  );
+// cache formatted data with keys for array like iteration
+const formattedData = massageData(data);
+formattedData.keys = Object.keys(formattedData);
+// generates strings for displaying app_key property values
 const createUsageContent = arr => {
-  return arr.map((el) => {
-    switch(el) {
-      case 'giving_tree':
-        return `🌲 Giving Tree`
-        break;
-      case 'console':
-        return `💻 Console`
-        break;
-      case 'community':
-        return `🏙️ Community`
-        break;
-      default: `🍺 ${formatReadableName(el)}`
-        return
-    }
-  }).join(' ')
-}
-
-const fieldGroupItem = (props) => {
+  return arr
+    .map(el => {
+      switch (el) {
+        case "giving_tree":
+          return `🌲 Giving Tree`;
+          break;
+        case "console":
+          return `💻 Console`;
+          break;
+        case "community":
+          return `🏙️ Community`;
+          break;
+        default:
+          `🍺 ${formatReadableName(el)}`;
+          return;
+      }
+    })
+    .join(" ");
+};
+// creates content for the main section
+const createFieldGroupItem = el => {
   return (
-    <div id={props.name} class='fieldgroupitem_div--outer'>
-      <h2 class='fieldgroupitem_h2'>{props.display_name}</h2>
-      <div>
-        <p class='item__prompt'>Type</p>
-        <p class='item__content'>{props.type}</p>
+    <div
+      id={el.name}
+      className="fieldgroupitem__div--outer"
+      key={`item_${el.name}`}
+    >
+      <h4 className="fieldgroupitem__h2">{formatReadableName(el.name)}</h4>
+      <div className="item__property">
+        <span className="item__prompt">Type:</span>
+        <span className="item__content">{el.data_type}</span>
       </div>
-      <div>
-        <p class='item__prompt'>Usage</p>
-        <p class='item__content'>{createUsageContent(props.usage)}</p>
+      <div className="item__property">
+        <span className="item__prompt">Usage:</span>
+        <span className="item__content">
+          {el.app_keys ? createUsageContent(el.app_keys) : "none"}
+        </span>
       </div>
-      <div>
-        <p class='item__prompt'>EverTrue Field Name</p>
-        <p class='item__content fieldname'></p>
+      <div className="item__property">
+        <span className="item__prompt">EverTrue Field Name:</span>
+        <span className="item__content fieldname">{el.name}</span>
       </div>
     </div>
-  )
-}
-
-const createLinks = arr => arr.map((el) => 
-    <li>
-      <a href={`#el.name`}>{el.formattedName}</a>
-    </li>
-  )
-
-const SideNav = () => {
-  return (d
-  <div id='sidenav'>
-   <ul>
-     {createLinks(formattedData)}
-   </ul>
-  </div>)
-}
-
+  );
+};
+// ----- React Components
+const SideNav = props => {
+  const { content } = props;
+  const createLinks = keys =>
+    keys.map(key => (
+      <div key={`sidenav__${key}`}>
+        <h2>{formatReadableName(content[key].name)}</h2>
+        {content[key].properties.map(el => (
+          <div key={`${key}_link_${el.name}`}>
+            <a href={`#${el.name}`}>{formatReadableName(el.name)}</a>
+          </div>
+        ))}
+      </div>
+    ));
+  return <div id="sidenav">{createLinks(content.keys)}</div>;
+};
 const Content = props => {
-  return (<div id='content'></div>)
-}
+  const { content } = props;
+  const createFieldGroups = keys =>
+    keys.map(key => (
+      <div key={`fieldgroup_${key}`}>
+        <h2>{formatReadableName(content[key].name)}</h2>
+        {content[key].properties.map(el => createFieldGroupItem(el))}
+      </div>
+    ));
+  return <div id="content">{createFieldGroups(content.keys)}</div>;
+};
+
+const dataContext = React.createContext({
+  active: "general",
+  ...formattedData
+});
 
 const App = () => {
   return (
-    <div className='App'>
-      <SideNav />
-      <Content />
-    </div>
+    <dataContext.Provider>
+      <div className="App">
+        <SideNav content={formattedData} />
+        <Content content={formattedData} />
+      </div>
+    </dataContext.Provider>
   );
 };
 
